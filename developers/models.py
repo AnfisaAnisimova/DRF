a66e8238d1f97876cb8007ae39d50fea11be2226
Anfisa <anfisochka900@gmail.com>
2022-01-23 16:33:16 +0400

    lesson_1. Developer model correction. UUID

--- developers/models.py
+++ developers/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from uuid import uuid4
 
 
 class Developer(models.Model):
+   uuid = models.UUIDField(primary_key=True, default=uuid4())
    username = models.CharField(max_length=64, verbose_name="Никнейм", unique=True)
    first_name = models.CharField(max_length=64, verbose_name="Имя")
    last_name = models.CharField(max_length=64, verbose_name="Фамилия")
