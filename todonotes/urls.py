c7275a7d7e37be2294b79ffb4266bfc8fa1779b8
AnfisaAnisimova <anfisochka900@gmail.com>
2022-03-08 23:15:29 +0400

    Lesson 6. Permissions and authorization

--- todonotes/urls.py
+++ todonotes/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from developers.views import DeveloperModelViewSet
 from todo.views import ProjectModelViewSet, ToDoModelViewSet
+from rest_framework.authtoken import views
 
 router = DefaultRouter()
 router.register("developers", DeveloperModelViewSet)
@@ -14,4 +15,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
